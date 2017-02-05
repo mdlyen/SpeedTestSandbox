7fb99a0fa19772f52c20d0a84f47326722ba872b
Mark Lyen <mark@marklyen.com>
2017-02-05 15:41:40 -0800

    Add Iteration Base for comparision.

---
+++ SpeedTestSandbox/TestClasses/IterationBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using SpeedTestSandbox.SandBoxClasses;
+
+namespace SpeedTestSandbox.TestClasses
+{
+    public class IterationBase : ISpeedTest
+    {
+        public long ElapsedTime { get; private set; }
+        public string ClassName { get; private set; }
+        public int Iterations => 150000;
+
+        void ISpeedTest.PerformTest()
+        {
+            ClassName = GetType().Name;
+
+            // Create stopwatch.
+            var sw = new Stopwatch();
+
+            // Start the stopwatch.
+            sw.Start();
+
+            // Perform the testing code.
+            var resultString = "";
+
+            for (var i = 0; i < Iterations; i++)
+            {
+                //resultString += i.ToString();
+            }
+
+            // Stop the stopwatch.
+            sw.Stop();
+
+            ElapsedTime = sw.ElapsedMilliseconds;
+        }
+    }
+}
